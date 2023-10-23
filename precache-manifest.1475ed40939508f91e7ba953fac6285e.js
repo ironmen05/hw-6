609cf734c38d439624ff63d50cb2f8553fbce762
Elaman Janyshbekov <elamanzanysbekov4@gmail.com>
2023-10-23 18:22:03 +0600

    Updates

---
+++ precache-manifest.1475ed40939508f91e7ba953fac6285e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34189fd442313aa93022033a54cd93c1",
+    "url": "/my-appgit%20remote%20add%20origin/index.html"
+  },
+  {
+    "revision": "d0d41ce629929ff57b54",
+    "url": "/my-appgit%20remote%20add%20origin/static/css/main.b6973ff7.chunk.css"
+  },
+  {
+    "revision": "ff27d552ba003cd4098b",
+    "url": "/my-appgit%20remote%20add%20origin/static/js/2.52e264af.chunk.js"
+  },
+  {
+    "revision": "d0d41ce629929ff57b54",
+    "url": "/my-appgit%20remote%20add%20origin/static/js/main.c908079a.chunk.js"
+  },
+  {
+    "revision": "c6814a358601e7b64a5a",
+    "url": "/my-appgit%20remote%20add%20origin/static/js/runtime~main.032e741a.js"
+  }
+]);
